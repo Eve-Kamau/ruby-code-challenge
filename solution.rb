3af7c0f0252e1d5179599cce7852322337578bad
Eve-Kamau <evelyne.kamau@student.moringaschool.com>
2023-04-03 21:31:51 +0300

    Code Challenge - Final Draft

--- solution.rb
+++ solution.rb
@@ -1,9 +1,10 @@
 # Please copy/paste all three classes into this file to submit your solution!
 require 'pry'
+
 class Author
   attr_reader :name
 
-  def initialize(name)
+  def initialize(name="Ava")
     @name = name
   end
 
@@ -18,16 +19,37 @@ class Author
     Article.all.select do |article|
     if article.author == self
       magazines << article.magazine
+    end 
+   end
+  end
+
+  def contributors
+    contributors = []
+    Article.all.select do |article|
+    if article.author == self
+      contributors << article.magazine
+   end 
+  end
+  end
+
+  def add_article(magazine, title)
+    Article.new(author: self, magazine: magazine, title: title)
+  end
+
+  def topic_areas
+    topic_areas = []
+    Article.all.select do |article|
+    if article.author == self
+      topic_areas << article.magazine.category
+   end 
+  end
 end 
 
-puts
-puts "AUTHOR CONSOLE"
-mike = Author.new
-puts mike.name
-agatha = Author.new("Agatha")
-puts agatha.name
+end 
+
 #------------------------------------------------------------
-  class Magazine
+
+class Magazine
     @@all = []
     attr_accessor :name, :category
   
@@ -46,23 +68,33 @@ puts agatha.name
         puts magazine.name
       end
     end
-  end
 
-  puts
-  puts "MAGAZINE CONSOLE"
-  digest = Magazine.new("Readers Digest", "News")
-  puts digest.name
-  puts digest.category
-  
-  upishi = Magazine.new("Upishi Bora", "Cooking")
-  puts upishi.name
-  puts upishi.category
-  
-  harvard = Magazine.new("Harvard Magazine", "Educational")
-  charisma = Magazine.new("Charisma Magazine", "Religion")
-  wired = Magazine.new("Wired", "Tech")
-  
-  puts Magazine.all
+    def find_by_name(name)
+      self.all.find_all do |magazine_name| 
+      self.magazine_name.name == name
+      end
+    end
+
+    def article_titles
+      article_titles = []
+      Article.all.select do |article|
+       if article.magazine.title == self
+        article_titles << article.title
+       end 
+      end
+    end 
+
+    def contributing_authors
+      contributing_authors = []
+     Article.all.select do |article|
+      if article.magazine.name == self
+      contributing_authors << article.author
+      end 
+     end
+    end
+
+end 
+
 #------------------------------------------------------------
 class Article
     @@all = []
@@ -92,23 +124,30 @@ class Article
     def magazine
       @magazine
      end 
-end
 
+    end
+
+puts Author 
+puts author1 = Author.new("Eric")
+puts author2 = Author.new("Maggie")
+puts author3 = Author.new("Adam")
+puts 
+
+puts Article
+puts article1 = Article.new("Simon", "Glory News", "The Reason")
+puts article2 = Article.new("Edna","Charisma","Experimental Faith")
+puts article3 = Article.new("Xiao", "Wired", "Apple Redefined")
 puts
-puts "ARTICLE CONSOLE"
-glory = Article.new
-puts glory.title 
-puts glory.author
-puts glory.magazine 
 
-charisma = Article.new("Charisma")
-wired = Article.new("Wired")
+puts Magazine
+puts mag1 = Magazine.new("Daily Tech News","Technology")
+puts mag2 = Magazine.new("Farmer News","Agriculture")
+puts mag3 = Magazine.new("Christianity", "Religion")
+puts 
 
 puts Article.all
 
 #------------------------------------------------------------
 
 binding.pry
-
-
 
